3d38a99e3c88570e56d48dec6050f2ad44486f1a
podmani <maxim@f1.nnov.ru>
2021-12-23 16:03:31 +0300

    ДЗ-5

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,11 +1,17 @@
 package org.example;
 
-import ch.qos.logback.classic.Logger;
-import org.junit.jupiter.api.Test;
-import org.slf4j.LoggerFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import java.time.Duration;
 
 
 /**
@@ -15,19 +21,105 @@ public class AppTest
 
 
 {
-    private static Logger logger = (Logger) LoggerFactory.getLogger(AppTest.class);
-    /**
-     * Rigorous Test :-)
-     */
+
+    static WebDriver webDriver;
+
+    @BeforeAll
+    static void setDriver() {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+        options.setPageLoadTimeout(Duration.ofSeconds(10));
+        webDriver = new ChromeDriver(options);
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @BeforeEach
+
+    void initMainPage() {
+        Assertions.assertDoesNotThrow( ()-> webDriver.navigate().to("https://www.fotosklad.ru/catalog/sale/"),
+                "Страница не доступна");
+    }
+
+
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        logger.error("My error");
-        assertTrue( true );
+    void TestSearchKey() {
+
+        WebElement webElement = webDriver.findElement(By.id("input-search-key"));
+        new WebDriverWait(webDriver, 5).until(ExpectedConditions.visibilityOf(webElement));
+
+
+        webElement.click();
+
+        Actions actions = new Actions(webDriver);
+        actions.sendKeys(webDriver.findElement(By.id("input-search-key")),"фото").click().build().perform();
+        System.out.println(webDriver.findElement(By.id("input-search-key")).getAttribute("value"));
+        Assertions.assertTrue(webDriver.findElement(By.id("input-search-key")).getAttribute("value").equals("фото"));
+
+       //webDriver.quit();
+}
+
+    @Test
+
+    void TestPositiveLogin() {
+        
+
+    WebElement webElement = webDriver.findElement(By.cssSelector(".box_catalog_prof > span"));
+    Actions actions = new Actions(webDriver);
+    actions.moveToElement(webDriver.findElement(By.cssSelector(".box_catalog_prof > span")));
+    new WebDriverWait(webDriver, 5).until(ExpectedConditions.visibilityOf(webElement));
+
+
+    webDriver.findElement(By.id("login_menu"));
+    actions.click(webDriver.findElement(By.id("login_menu"))).build().perform();
+
+    actions.click(webDriver.findElement(By.cssSelector(".fancybox-overlay")))
+            .sendKeys(webDriver.findElement(By.name("USER_LOGIN")),"maxim@f1.nnov.ru")
+            .sendKeys(webDriver.findElement(By.name("USER_PASSWORD")),"856506").build().perform();
+
+    actions.click(webDriver.findElement(By.name("Login"))).build().perform();
+
+    Assertions.assertTrue(webDriver.findElement(By.name("USER_LOGIN")).getAttribute("value").equals("maxim@f1.nnov.ru"));
+       /* webDriver.findElement(By.cssSelector(".box_catalog_prof > p"));
+        Actions actions2 = new Actions(webDriver);
+        actions.moveToElement(webElement);
+       new WebDriverWait(webDriver, 5).until(ExpectedConditions.visibilityOf(webElement));
+       webDriver.findElement(By.id("autorization"));
+        webDriver.findElement(By.linkText("[выйти]"));
+       actions2.click(webDriver.findElement(By.linkText("[выйти]"))).build().perform();*/
+    webDriver.quit();
     }
 
     @Test
-    void MyTest() {
-        System.out.println("My test");
+
+    void TestNegativeLogin() {
+
+
+        WebElement webElement = webDriver.findElement(By.cssSelector(".box_catalog_prof > span"));
+        Actions actions = new Actions(webDriver);
+        actions.moveToElement(webElement);
+        new WebDriverWait(webDriver, 5).until(ExpectedConditions.visibilityOf(webElement));
+
+
+        webDriver.findElement(By.id("login_menu"));
+        actions.click(webDriver.findElement(By.id("login_menu"))).build().perform();
+
+        actions.click(webDriver.findElement(By.cssSelector(".fancybox-overlay")))
+                .sendKeys(webDriver.findElement(By.name("USER_LOGIN")),"maxim@f1.nn.ru")
+                .sendKeys(webDriver.findElement(By.name("USER_PASSWORD")),"856506").build().perform();
+
+        actions.click(webDriver.findElement(By.name("Login"))).build().perform();
+        //System.out.println(webDriver.findElement(By.name("USER_LOGIN")).getAttribute("value"));
+        Assertions.assertFalse(webDriver.findElement(By.name("USER_LOGIN")).getAttribute("value").equals("maxim@f1.nnov.ru"));
+
+
+        webDriver.quit();
     }
+    /*@AfterAll
+    static void exit() {
+    webDriver.quit();
+    }*/
 }
